6c5a1225e9d4e2cc737d70dae1e991ed8f5688a0
thiaguinho700 <thiagobreakgo@gmail.com>
2024-12-01 13:00:07 -0300

    Att

---
+++ src/graphics/BarChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <BarChart
+      v-if="chartData && chartOptions"
+      :chart-data="chartData"
+      :chart-options="chartOptions"
+      style="width: 800px; height: 500px"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  registerables 
+} from "chart.js";
+
+// Registrar os componentes do Chart.js
+ChartJS.register(
+  ...registerables 
+);
+
+export default defineComponent({
+  name: "BarChartComponent", // Nome atualizado
+  components: {
+    BarChart: Bar, // Certifique-se de usar o componente correto
+  },
+  setup() {
+    
+      const testeData = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            labels: "Data 1",
+            backgroundColor: "#42A5F5",
+            data: [40, 20, 12, 39, 10, 40],
+          },
+        ],
+      }      
+    return {testeData}
+  },
+});
+</script>
+
+<style>
+/* Adicione estilos específicos, se necessário */
+</style>
